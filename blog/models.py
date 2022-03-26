9ee221808e3ba5875a1a19383683ec861c2368d1
kevlarr <6306496+kevlarr@users.noreply.github.com>
2022-03-26 19:53:57 -0400

    add Post and Comment models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,54 @@
+# from django.contrib.auth.models import User
+from django.contrib import auth
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    """
+    A blog post
+    """
+
+    class Meta:
+        ordering = ['-created_at']
+
+    author = models.ForeignKey(
+        auth.get_user_model(),
+        null=False,
+        on_delete=models.RESTRICT,
+    )
+    title = models.TextField(
+        null=False,
+        help_text='The title of the blog post',
+    )
+    text = models.TextField(
+        null=False,
+        help_text='The contents of the blog post',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+class Comment(models.Model):
+    """
+    A comment on a blog post
+    """
+
+    class Meta:
+        ordering = ['-created_at']
+
+    author = models.ForeignKey(
+        auth.get_user_model(),
+        null=False,
+        on_delete=models.RESTRICT,
+    )
+    post = models.ForeignKey(
+        'Post',
+        null=False,
+        on_delete=models.RESTRICT,
+    )
+    text = models.TextField(
+        null=False,
+        help_text='The contents of the comment',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
