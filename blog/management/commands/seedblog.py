90b83278277117fcafdce996a0f861a8222d8e3e
kevin <6306496+kevlarr@users.noreply.github.com>
2022-03-26 20:12:47 -0400

    Set up Post and Comment models (#13)
    * Add Post and Comment models
    
    * Add new command `seedblog`

---
+++ blog/management/commands/seedblog.py
@@ -0,0 +1,36 @@
+"""
+Exposes a command to generate test data for the blog models,
+as well as a handful of authors
+
+See: https://docs.djangoproject.com/en/4.0/howto/custom-management-commands/
+"""
+from django.contrib import auth
+from django.core.management.base import BaseCommand
+from django_seed import Seed
+
+from blog.models import Post, Comment
+
+
+USER = auth.get_user_model()
+
+
+class Command(BaseCommand):
+    """
+    Blog model seeder command
+    """
+
+    help = 'Seeds the blog with sample data'
+
+    def __init__(self):
+        self.seeder = Seed.seeder()
+
+    def handle(self, *args, **options):
+        """
+        Seeds users, posts, and comments such that:
+            - Some users SHOULD have multiple posts and comments
+            - Some posts SHOULD have multiple comments
+        """
+        self.seeder.add_entity(USER, 5)
+        self.seeder.add_entity(Post, 15)
+        self.seeder.add_entity(Comment, 45)
+        self.seeder.execute()
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,62 @@
+"""
+Blog models
+"""
+from django.contrib import auth
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    A blog post
+    """
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author_id', 'title'],
+                name='blog_post_author_id_title_uniq',
+            ),
+        ]
+        ordering = ['-created_at']
+
+    author = models.ForeignKey(
+        auth.get_user_model(),
+        null=False,
+        on_delete=models.RESTRICT,
+    )
+    title = models.TextField(
+        null=False,
+        help_text='The title of the blog post',
+    )
+    text = models.TextField(
+        null=False,
+        help_text='The contents of the blog post',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    """
+    A comment on a blog post
+    """
+
+    class Meta:
+        ordering = ['-created_at']
+
+    author = models.ForeignKey(
+        auth.get_user_model(),
+        null=False,
+        on_delete=models.RESTRICT,
+    )
+    post = models.ForeignKey(
+        'Post',
+        null=False,
+        on_delete=models.RESTRICT,
+    )
+    text = models.TextField(
+        null=False,
+        help_text='The contents of the comment',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
