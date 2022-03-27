256f0cb08358bc60b2c63e71d65e78f4a24a8faa
kevin <6306496+kevlarr@users.noreply.github.com>
2022-03-26 23:17:33 -0400

    Set up blog details page (#19)

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="blog-index"),
+    path('', views.index, name='blog-index'),
+    path('<int:post_id>/', views.details, name='blog-details'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,13 @@ def index(request):
         template_name='index.html',
         context={'headline': headline, 'remaining': remaining},
     )
+
+
+def details(request, post_id):
+    post = queries.post(post_id)
+
+    return render(
+        request=request,
+        template_name='details.html',
+        context={'post': post, 'comments': post.comments},
+    )
