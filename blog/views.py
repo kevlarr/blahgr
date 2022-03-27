72e2eba26f1a61181cb0d840315ad64c7948ad17
kevlarr <6306496+kevlarr@users.noreply.github.com>
2022-03-26 22:19:12 -0400

    basic index page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from . import queries
+
 
 def index(request):
+    headline = queries.headline_post()
+    remaining = queries.post_summaries(offset=1)
+
     return render(
         request=request,
         template_name='index.html',
+        context={'headline': headline, 'remaining': remaining},
     )
